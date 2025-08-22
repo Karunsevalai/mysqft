331af9031db0455fe5b6ff260e23a4f6f7ceb2d8
Hari <haranharan0784@gmail.com>
2025-08-22 21:36:42 +0530

    mysqft landing page

---
+++ index.js
@@ -0,0 +1,33 @@
+function sendForm(formData) {
+  return new Promise((resolve, reject) => {
+    fetch("http://127.0.0.1:5000/submit", {
+      method: "POST",
+      body: formData,
+     // credentials: "include" // only if you need cookies
+    })
+    .then(response => {
+      if (!response.ok) {
+        reject("Server returned " + response.status);
+      }
+      return response.json();
+    })
+    .then(data => resolve(data))
+    .catch(error => reject(error));
+  });
+}
+
+// Usage
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+
+  sendForm(formData)
+    .then(data => {
+      console.log("Server response:", data);
+      alert("✅ " + data.message);
+    })
+    .catch(err => {
+      console.error("Error:", err);
+      alert("❌ Failed: " + "Server Error!, please try later");
+    });
+});
